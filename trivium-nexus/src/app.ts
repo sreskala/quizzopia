b98fd15417696c4dcda1b0771e427956b7f29a65
Samuel Reskala <sreskala@Samuels-Air.attlocal.net>
2023-08-22 16:54:51 -0400

    adding index to trivium

---
+++ trivium-nexus/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@reskalaware/enigma-essentials';
+// import { createTicketRouter } from './routes/new';
+// import { showTicketRouter } from './routes/show';
+// import { updateTicketRouter } from './routes/update';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    name: 'auth-session',
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+app.use(currentUser);
+
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(updateTicketRouter)
+
+app.get('*', async (req: Request, res: Response) => {
+   throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
